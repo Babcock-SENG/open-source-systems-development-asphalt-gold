4ee8a3a28b42b6b3e60a7535c23efe60b5a263c4
Boluthe <bakaretioluwani@icloud.com>
2024-11-08 01:13:14 +0100

    admin panel

---
+++ components/admin_header.php
@@ -0,0 +1,61 @@
+<!-- header section starts -->
+
+<header class="header">
+
+    <section class="flex">
+
+        <a href="dashboard.php" class="logo">ADMIN.</a>
+
+        <form action="search_page.php" method="post" class="searchform">
+            <input type="text" placeholder="search here......" required
+                maxlength="100" name="search_box">
+            <button type="submit" class="fas fas-search" name="search_btn"></button>
+        </form>
+
+        <div class="/icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <div id="search -btn" class="fas fa-search"></div>
+            <div id="user-btn" class="fas fa-user"></div>
+            <div id="toggle-btn" class="fas fa-sun"></div>
+
+        </div>
+
+        <div class="profile">
+            <?php
+            $select_profile = $conn->prepare("SELWCT * FROM 'tutors' WHERE id
+        = ?");
+            $select_profile->execute([$user_id]);
+            if ($select_profile->rowCount() > 0) {
+                $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+            ?>
+                <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+                <h3><?= $fetch_profile['name']; ?></h3>
+                <span><?= $fetch_profile['profession']; ?></span>
+                <a href="profile.php" class="btn">view profile</a>
+                <div class="flex-btn">
+                    <a href="login.php" class="option-btn">login</a>
+                    <a href="register.php" class="option-btn">register</a>
+                </div>
+                <a href="../components/admin_logout.php" onclick="return comfirm
+                ('logout from this website?');" class="delete-btn">logout</a>
+            <?php
+            } else {
+
+            ?>
+                <h3 style="text-align: center;"> please login first</h3>
+                <a href="login.php" class="option-btn">login</a>
+                <a href="register.php" class="option-btn">register</a>
+
+
+            <?php
+
+            }
+
+            ?>
+        </div>
+
+    </section>
+
+</header>
+
+<!-- header section ends -->
