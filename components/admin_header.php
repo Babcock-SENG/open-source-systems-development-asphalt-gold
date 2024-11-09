39b06f54af57909d5f0f0ef536ef6102dd08bae5
Boluthe <bakaretioluwani@icloud.com>
2024-11-09 09:47:53 +0100

    admin register and login fixed

--- components/admin_header.php
+++ components/admin_header.php
@@ -59,3 +59,52 @@
 </header>
 
 <!-- header section ends -->
+
+<div class="side-bar">
+    <div class="profile">
+        <?php
+        $select_profile = $conn->prepare("SELWCT * FROM 'tutors' WHERE id
+        = ?");
+        $select_profile->execute([$user_id]);
+        if ($select_profile->rowCount() > 0) {
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+        ?>
+            <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+            <h3><?= $fetch_profile['name']; ?></h3>
+            <span><?= $fetch_profile['profession']; ?></span>
+            <a href="profile.php" class="btn">view profile</a>
+            <div class="flex-btn">
+                <a href="login.php" class="option-btn">login</a>
+                <a href="register.php" class="option-btn">register</a>
+            </div>
+            <a href="../components/admin_logout.php" onclick="return comfirm
+                ('logout from this website?');" class="delete-btn">logout</a>
+        <?php
+        } else {
+
+        ?>
+            <h3 style="text-align: center;"> please login first</h3>
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+    </div>
+<?php
+        }
+?>
+</div>
+
+<nav class="navbar">
+    <a href="dashboard.php"><i class="fas fa home"></i><span>home </span></a>
+    <a href="playlist.php"><i class="fas fa-bars-staggered"></i>
+        <span>playlists</span></a>
+    <a href="contents.php"><i class="fas fa-graduation-cap">
+        </i><span>contents</span></a>
+    <a href="comments.php"><i class="fas fa-comment"></i>
+        <span>comments</span></a>
+    <a href="../components/admin_logout.php" onclick="return comfirm
+                ('logout from this website?');" class="delete-btn">
+        <i class="fas fa-right-from-bracket"> </i><span>logout</span></a>
+</nav>
+
+</div>
+
+<!-- sidebar section starts -->
---
+++ js/admin_script.js
@@ -0,0 +1,14 @@
+let footer = document.querySelector(".footer");
+let body = document.body;
+let profile = document.querySelector(".header . flex .profile");
+let sideBar = document.querySelector(".side-bar");
+
+document.querySelector("#user-btn").onclick = () => {
+  profile.classList.toggle("active");
+};
+
+document.querySelector("wk");
+
+window.onscroll = () => {
+  profile.classList.remove("active");
+};
