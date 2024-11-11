441191ce7680601d58bfac1257833b88edeee55b
Odetundemay <oluwamayowa.c.odetunde@gmail.com>
2024-11-11 10:15:43 +0100

    Register form styles

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,21 +9,21 @@ if(isset($_COOKIE['tutor_id'])){
    header('location:login.php');
 }
 
-$select_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
-$select_contents->execute([$tutor_id]);
-$total_contents = $select_contents->rowCount();
+$count_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
+$count_contents->execute([$tutor_id]);
+$total_contents = $count_contents->rowCount();
 
-$select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
-$select_playlists->execute([$tutor_id]);
-$total_playlists = $select_playlists->rowCount();
+$count_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+$count_playlists->execute([$tutor_id]);
+$total_playlists = $count_playlists->rowCount();
 
-$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
-$select_likes->execute([$tutor_id]);
-$total_likes = $select_likes->rowCount();
+$count_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
+$count_likes->execute([$tutor_id]);
+$total_likes = $count_likes->rowCount();
 
-$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
-$select_comments->execute([$tutor_id]);
-$total_comments = $select_comments->rowCount();
+$count_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
+$count_comments->execute([$tutor_id]);
+$total_comments = $count_comments->rowCount();
 
 ?>
 
