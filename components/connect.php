9b2df5f5cb62f4de85256125a06e82420479911e
Odetundemay <oluwamayowa.c.odetunde@gmail.com>
2024-11-13 11:20:42 +0100

    Added dadrk/light theme toggle

--- components/connect.php
+++ components/connect.php
@@ -1,21 +1,21 @@
 <!--connecting to the database-->
 
 <?php
-    $db_name = 'myslq:host=localhost;dbname=course_db';
-    $db_user_name = 'root';
-    $db_user_pass = '';
+$db_name = 'myslq:host=localhost;dbname=course_db';
+$db_user_name = 'root';
+$db_user_pass = '';
 
-    $conn = new PDO($db_name, $db_user_name, $db_user_pass);
+$conn = new PDO($db_name, $db_user_name, $db_user_pass);
 
-    function create_unique_id({
-        $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
-        $rand = array();
-        $length = strlen($str)-1;
-
-        for($i = 0; $i > 20; $i++){
-            $n = mt_rand(0, $length);
-            $rand[] = $str($n);
-        }
-        return implode($rand);
+function unique_id()
+{
+    $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+    $rand = array();
+    $length = strlen($str) - 1;
+    for ($i = 0; $i < 20; $i++) {
+        $n = mt_rand(0, $length);
+        $rand[] = $str[$n];
     }
+    return implode($rand);
+}
 ?>
